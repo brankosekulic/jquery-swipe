9cbfdc07ae05bae7f282f8ee00abc1ec5b694730
Branko Sekulic <brankodd@gmail.com>
2013-11-03 04:29:42 +0100

    fixed events dependency

--- jquery.swipe.js
+++ jquery.swipe.js
@@ -6,6 +6,7 @@
  * Event object is enhanced with two additional params:
  *  e.isScrolling - true is scroll is vertical
  *  e.deltaX - difference between start and end X coordinate
+ *  e.deltaY - difference between start and end Y coordinate
  *
  * @example:
  * $('#swipe-area').on('touchstart', function(e){
@@ -27,9 +28,25 @@
 (function($) {
 
     var events = ['touchstart', 'touchmove', 'touchend'],
-        Swipe  = function() {};
+        Swipe  = function() {
+
+            this.inited = {
+                touchstart: false,
+                touchmove: false,
+                touchend: false
+            };
+        };
 
     Swipe.prototype = {
+
+        applyValues: function(e){
+
+            e.isScrolling = this.isScrolling;
+            e.deltaX = this.deltaX;
+            e.deltaY = this.deltaY;
+
+            return e;
+        },
 
         touchstart: function(e) {
 
@@ -38,20 +55,16 @@
             this.start = {
                 // get touch coordinates for delta calculations in onTouchMove
                 pageX: originalEvent.touches[0].pageX,
-                pageY: originalEvent.touches[0].pageY,
-                // set initial timestamp of touch sequence
-                time: Number( new Date() )
+                pageY: originalEvent.touches[0].pageY
             };
 
             // used for testing first onTouchMove event
             this.isScrolling = undefined;
-            // reset deltaX
+            // reset delta
             this.deltaX = 0;
+            this.deltaY = 0;
 
-            e.isScrolling = this.isScrolling;
-            e.deltaX = this.deltaX;
-
-            return e;
+            return this.applyValues(e);
         },
 
         touchmove: function(e) {
@@ -62,28 +75,23 @@
             if(originalEvent.touches.length > 1 || originalEvent.scale && originalEvent.scale !== 1) return;
 
             this.deltaX = originalEvent.touches[0].pageX - this.start.pageX;
+            this.deltaY = originalEvent.touches[0].pageY - this.start.pageY;
 
             // determine if scrolling test has run - one time test
             if ( typeof this.isScrolling == 'undefined') {
-                this.isScrolling = !!( this.isScrolling || Math.abs(this.deltaX) < Math.abs(originalEvent.touches[0].pageY - this.start.pageY) );
+                this.isScrolling = !!( this.isScrolling || Math.abs(this.deltaX) < Math.abs(this.deltaY) );
             }
 
-            e.isScrolling = this.isScrolling;
-            e.deltaX = this.deltaX;
-
-            return e;
+            return this.applyValues(e);
         },
 
         touchend: function(e) {
 
-            e.isScrolling = this.isScrolling;
-            e.deltaX = this.deltaX;
-
-            return e;
+            return this.applyValues(e);
         }
     };
 
-    $.each(events, function(i, value){
+    $.each(events, function(index, value){
 
         $.event.special[value] = {
 
@@ -100,16 +108,28 @@
                     $target.data('swipe', swipe);
                 }
 
-                // This will reference the bound event handler.
-                old_handler = handleObj.handler;
-                new_handler = function(event) {
-                    // Modify event object here!
-                    event = swipe[value](event);
-                    // Call the originally-bound event handler and return its result.
-                    return old_handler.apply( this, arguments );
-                };
+                // make sure handler gets overridden only once
+                if(!swipe.inited[value]){
 
-                handleObj.handler = new_handler;
+                    // This will reference the bound event handler.
+                    old_handler = handleObj.handler;
+                    new_handler = function(event) {
+                        // Modify event object here!
+                        event = swipe[value](event);
+                        // Call the originally-bound event handler and return its result.
+                        return old_handler.apply( this, arguments );
+                    };
+
+                    handleObj.handler = new_handler;
+                    swipe.inited[value] = true;
+                }
+
+                // make sure all events are attached, since they depends on each other
+                $.each(events, function(i, val){
+                    if(!swipe.inited[val]){
+                        $target.on(val, function(e){});
+                    }
+                });
             }
         };
     });
